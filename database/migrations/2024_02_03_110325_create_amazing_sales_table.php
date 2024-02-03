c1fabc295777869719caaa014d0d29cee2018bbf
amirrezaraadi <amirrezaraadi@yahoo.com>
2024-02-03 15:21:55 +0330

    add migration  coupon

---
+++ database/migrations/2024_02_03_110325_create_amazing_sales_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('amazing_sales', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')
+                ->constrained('products')
+                ->cascadeOnUpdate()
+                ->cascadeOnUpdate();
+            $table->integer('percentage');
+            $table->tinyInteger('status')->default(0);
+            $table->timestamp('start_date')->useCurrent();
+            $table->timestamp('end_date')->useCurrent();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('amazing_sales');
+    }
+};
---
+++ database/migrations/2024_02_03_110440_create_common_discount_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('common_discount', function (Blueprint $table) {
+//            todo -> تخفیف عمومی بر روی همه محصولات
+            $table->id();
+            $table->string('title');
+            $table->integer('percentage');
+            $table->unsignedBigInteger('discount_ceiling')->nullable();
+            $table->unsignedBigInteger('minimal_order_amount')->nullable();
+            $table->tinyInteger('status')->default(0);
+            $table->timestamp('start_date')->useCurrent();
+            $table->timestamp('end_date')->useCurrent();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('common_discount');
+    }
+};
