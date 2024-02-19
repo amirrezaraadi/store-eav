d5d7fa83c2cc9d3904ae0237c5a5b169b7665d9c
amirrezaraadi <amirrezaraadi@yahoo.com>
2024-02-19 13:01:26 +0330

    add variant to project

---
+++ app/Models/Panel/Variant.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Panel;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Variant extends Model
+{
+    use HasFactory;
+}
